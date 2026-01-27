5a111202a9a1f9244b5874d730d396287b0effb0
Leander Perez <leanderperez@gmail.com>
2026-01-27 14:56:21 -0400

    Fix URL mapping and update button link in datatable template for report creation

--- servicio/urls.py
+++ servicio/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import LogoutView
 from .views import (crear_reporte, modificar_reporte, acceso_denegado, datatable)
 
 urlpatterns = [
+    path('', datatable, name='datatable'),
     path('crear-reporte/', crear_reporte, name='crear_reporte'),
     path('modificar-reporte/<int:pk>/', modificar_reporte, name='modificar_reporte'),
     path('acceso-denegado/', acceso_denegado, name='acceso_denegado'),
